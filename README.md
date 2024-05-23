ac4e93905a2baf123c10368627844f0235e1b8b1
kurorinchan <kurorinchan@users.noreply.github.com>
2024-05-23 10:23:31 +0900

    Handle more long URL formats
    It used to handle dp only. Now it handles gp, exec, and o as well.
    Added a function to handle them.
    Changed README so that unit testing in vscode works.

--- README.md
+++ README.md
@@ -8,3 +8,22 @@ https://kurorinchan.github.io/shorten-url-leptos/
 RUSTFLAGS=--cfg=web_sys_unstable_apis trunk serve --open
 ```
 
+# VScode
+When using vscode, put the following in workspace's `settings.json`
+
+```json
+{
+    "rust-analyzer.rustfmt.overrideCommand": [
+        "leptosfmt",
+        "--stdin",
+        "--rustfmt"
+    ],
+    "rust-analyzer.cargo.extraEnv": {
+        "RUSTFLAGS": "--cfg=web_sys_unstable_apis"
+    },
+    // for tests to build.
+    "rust-analyzer.runnables.extraEnv": {
+        "RUSTFLAGS": "--cfg=web_sys_unstable_apis"
+    }
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,51 @@
 use leptos::*;
 use url::Url;
 
+/// There are 4 ways amazon URL contain product id.
+/// 1. <any>/dp/<id>
+/// 2. <domain>/gp/product/<id>
+/// 3. <domain>/exec/obidos/asin/<id>
+/// 4. <domain>/o/ASIN/<id>
+/// This function returns the <id> within the above patterns.
+fn get_product_id(url: &str) -> Option<String> {
+    let url = Url::parse(url).ok()?;
+
+    const DP: &str = "dp";
+    const GP: &str = "gp";
+    const EXEC: &str = "exec";
+    const O: &str = "o";
+    let parts = url.path().split('/').collect::<Vec<&str>>();
+    for (i, part) in parts.iter().enumerate() {
+        if part == &DP {
+            if i + 1 >= parts.len() {
+                break;
+            }
+            return Some(parts[i + 1].to_owned());
+        }
+        if part == &GP {
+            if i + 2 >= parts.len() {
+                break;
+            }
+            return Some(parts[i + 2].to_owned());
+        }
+        if part == &O {
+            if i + 2 >= parts.len() {
+                break;
+            }
+            return Some(parts[i + 2].to_owned());
+        }
+
+        if part == &EXEC {
+            if i + 3 >= parts.len() {
+                break;
+            }
+            return Some(parts[i + 3].to_owned());
+        }
+    }
+
+    None
+}
+
 #[component]
 fn App() -> impl IntoView {
     let (url, set_url) = create_signal(String::new());
@@ -18,6 +63,7 @@ fn App() -> impl IntoView {
             .expect("<input> should be mounted")
             .value();
 
+        let product_id = get_product_id(&value);
         let parsed_url = Url::parse(&value);
         let parsed_url = match parsed_url {
             Ok(parsed_url) => parsed_url,
@@ -27,15 +73,12 @@ fn App() -> impl IntoView {
             }
         };
 
-        let mut path_iter = parsed_url.path().split('/');
-
-        if let Some(_part) = path_iter.find(|part| part == &"dp") {
-            let product_id = path_iter.next();
+        if let Some(product_id) = product_id {
             let short_url = parsed_url.scheme().to_owned()
                 + "://"
                 + parsed_url.host_str().unwrap()
                 + "/dp/"
-                + product_id.unwrap();
+                + &product_id;
             processed_url_set.set(short_url);
             set_status.set("success".to_string());
         } else {
@@ -110,4 +153,45 @@ fn main() {
         document().body().expect("should have body"),
         move || view! { <App/> },
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_product_id_dp() {
+        let url = "https://www.amazon.co.jp/dp/B01N9KXH9E";
+        assert_eq!(get_product_id(url), Some("B01N9KXH9E".to_string()));
+    }
+
+    #[test]
+    fn test_get_product_id_dp_with_extra_stuff() {
+        let url = "https://www.amazon.co.jp/Comomed-%E3%83%AD%E3%83%BC%E3%83%AB%E8%87%AA%E5%B7%B1%E7%B2%98%E7%9D%80%E5%8C%85%E5%B8%AF%E7%99%BD-5cm-4-5-%E5%BC%BE%E6%80%A7%E5%8C%85%E5%B8%AF%E3%80%81%E4%B8%8D%E7%B9%94%E5%B8%83%E3%80%81%E3%82%B9%E3%83%9D%E3%83%BC%E3%83%84%E3%83%86%E3%83%BC%E3%83%97%E3%80%81%E7%8D%A3%E5%8C%BB%E5%8C%85%E8%A3%85%E3%80%81%E5%8C%85%E5%B8%AF%E5%8C%85%E5%B8%AF%E3%80%81%E6%95%8F%E6%84%9F%E8%82%8C%E3%81%AB%E9%81%A9%E3%81%97%E3%81%A6%E3%81%84%E3%81%BE%E3%81%99%E3%80%82/dp/B07DVHW5QD/ref=sr_1_1_sspa?dib=eyJ2IjoiMSJ9.WJ3pdApniFZc9dR5oLkjieQhitNIeI_JnK-LBk7R1Rcz6Vvza4f8irbDEKCj4lnJaJ6JMKNIQWFqizyTCRUHGipHK6uk1OsTAK43sZMiNj6p-_aYystOXhBEhKt4NfAf9LgAOs-49EW0I0cLqUN7-SqjB_JtLLD2j0oIid2bk97CY16YJOzcR344VXXc-WOwmhaIa9K8fVtQZato6DNaBoHNDsMPKWm1HdbboUofL8pvIW0h2hI3TueyAqusC95WRFU02vgasqxM8_iWORYz1nC4zUfCLwrMsjtpAXQGvHQ.Yo3JEW1BJJOU9FJpDUS2SjigvNbYimbp4284WQmy1qc&dib_tag=se&keywords=%E5%8C%85%E5%B8%AF&qid=1716426773&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1";
+        assert_eq!(get_product_id(url), Some("B07DVHW5QD".to_string()));
+    }
+
+    #[test]
+    fn test_get_product_id_gp() {
+        let url = "https://www.amazon.co.jp/gp/product/B01N9KXH9E";
+        assert_eq!(get_product_id(url), Some("B01N9KXH9E".to_string()));
+    }
+
+    #[test]
+    fn test_get_product_id_gp_with_extra_stuff() {
+        let url = "https://www.amazon.co.jp/gp/product/B099J7WNV2/ref=ox_sc_saved_image_2?smid=A22RFVFOGUYRPO&psc=1";
+        assert_eq!(get_product_id(url), Some("B099J7WNV2".to_string()));
+    }
+
+    #[test]
+    fn test_get_product_id_exec() {
+        let url = "https://www.amazon.co.jp/exec/obidos/asin/B01N9KXH9E";
+        assert_eq!(get_product_id(url), Some("B01N9KXH9E".to_string()));
+    }
+
+    #[test]
+    fn test_get_product_id_o() {
+        let url = "https://www.amazon.co.jp/o/ASIN/B01N9KXH9E";
+        assert_eq!(get_product_id(url), Some("B01N9KXH9E".to_string()));
+    }
 }
